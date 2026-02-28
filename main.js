f68cb25ffd0d6c4d48ba08d7fb8c44525a420078
HYEJIN KWON <chahaa0189@gmail.com>
2026-02-28 23:27:22 +0900

    feat : pagination 기능 및 이벤트 기능 구현

--- main.js
+++ main.js
@@ -7,11 +7,24 @@ let keywordButton = document.getElementById('keyword-btn')
 let bars = document.querySelector('.fa-bars')
 let sideButton = document.querySelectorAll('.side-menu-area a')
 
+let totalResults = 0
+let page = 1
+const pageSize = 10
+const groupSize  =5
+
+
 
 let deleteButton = document.querySelector('.fa-x')
 let menus = document.querySelectorAll('.menus button')
 
-
+userInput.addEventListener("focus",()=>{
+    userInput.value = ""
+})
+userInput.addEventListener("keydown",(event)=>{
+    if(event.key === "Enter"){
+        getNewsByKeyword()
+    }
+})
 //카테고리 버튼 
 menus.forEach((item)=>
 item.addEventListener("click",(event)=>{
@@ -47,18 +60,22 @@ const getNews = async ()=>{
 
     
     try {
-         
+        url.searchParams.set("page",page)
+        url.searchParams.set("pageSize",pageSize)
         let response = await fetch(url)
         console.log("response",response)
         let data = await response.json() //파일 형태 확장자 
+        
            console.log("data",data)
         if(response.status === 200){
            if(data.articles.length === 0){
                     throw new Error("No matches for your search")
                 }
                 newsList = data.articles
-                console.log(newsList)
+                totalResults =data.totalResults
+                console.log(newsList,totalResults)
                 render()
+                paginationRender()
     
         }else{
             throw new Error(data.message)
@@ -73,6 +90,7 @@ const getNews = async ()=>{
 
 
 const getLatestNews = async ()=>{
+    page = 1
     url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?`)
     getNews()
 }
@@ -82,6 +100,7 @@ getLatestNews()
 
 //카테고리별 뉴스
 const getNewsByCategory= async (event)=>{
+    page = 1
     let category = event.target.textContent.toLowerCase()
     url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?category=${category}`)
     getNews()
@@ -91,8 +110,10 @@ const getNewsByCategory= async (event)=>{
 //키워드별 뉴스
 
 const getNewsByKeyword = async ()=>{
+    page = 1
     let keyword = userInput.value
     url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?q=${keyword}`)
+    userInput.value = ""
     getNews()
 }
 
@@ -136,4 +157,48 @@ const errorRender = (error)=>{
 </div>
     `
     document.getElementById('news-list').innerHTML =errorHTML
+      document.querySelector(".pagination").innerHTML = ""
+}
+
+const paginationRender = ()=>{
+
+    let totalPage = Math.ceil(totalResults/pageSize)
+    
+    console.log(totalPage)
+    let pageGroup = Math.ceil(page/groupSize)
+    console.log(pageGroup)
+    let lastPage = groupSize*pageGroup
+    if(lastPage>totalPage){
+        lastPage = totalPage
+    }
+    let firstPage = lastPage -(groupSize-1) < 1 ? 1 : lastPage -(groupSize-1)
+    console.log(lastPage,firstPage)
+
+
+    let paginationHTML = ``
+    if(page>1){
+        paginationHTML=`<li class="page-item"><a class="page-link" onclick="moveToPage(1)">&lt;&lt</a></li>
+    <li class="page-item"><a class="page-link" onclick="moveToPage(${page-1})">&lt;</a></li>`
+    }
+    
+    for(let i = firstPage;i<=lastPage;i++){
+        paginationHTML+= `  
+        <li class="page-item"><a class="page-link ${i === page ?"active": ""}" onclick="moveToPage(${i})">${i}</a></li>
+   `    
+    }
+
+    if(page<totalPage){
+
+        paginationHTML+=` <li class="page-item"><a class="page-link" onclick="moveToPage(${page+1})" >&gt;</a></li>
+        <li class="page-item"><a class="page-link" onclick="moveToPage(${totalPage})" > &gt;&gt; </a></li>
+        `
+    }
+   
+
+    document.querySelector('.pagination').innerHTML = paginationHTML
+}
+
+const moveToPage = (num)=>{
+    page = num
+    getNews()
 }
